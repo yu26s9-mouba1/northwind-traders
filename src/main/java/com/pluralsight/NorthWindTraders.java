2659d1a7ab5415524fbd762949c79726b642481e
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-07 19:32:33 -0400

    Modified the query and displayed all products

--- src/main/java/com/pluralsight/NorthWindTraders.java
+++ src/main/java/com/pluralsight/NorthWindTraders.java
@@ -6,7 +6,7 @@ public class NorthWindTraders {
 
     String productsQuery = """
            
-            select productName
+            select productID, productName, unitPrice, unitsInstock
           
             from products
             
@@ -25,9 +25,34 @@ public class NorthWindTraders {
         PreparedStatement preparedStatement = connection.prepareStatement(productsQuery);
         ResultSet resultSet = preparedStatement.executeQuery();
 
+        //Header
+        System.out.printf("%-5s %-30s %-10s %-10s%n",
+                "Id", "Name", "Price", "Stock");
+        System.out.println("----------------------------------------------------------------------------");
+
+
         while (resultSet.next()) {
-            String productName = resultSet.getString("productName");
-            System.out.println("Product Name: " + productName);
+            int productId = resultSet.getInt("ProductID");
+            String productName = resultSet.getString("ProductName");
+            double unitPrice = resultSet.getDouble("UnitPrice");
+            int unitsInStock = resultSet.getInt("UnitsInStock");
+
+
+            //Stacked Format
+//            System.out.println("Product ID: " + productId);
+//            System.out.println("Product Name: " + productName);
+//            System.out.println("Unit Price: " + unitPrice);
+//            System.out.println("Units In Stock: " + unitsInStock);
+//            System.out.println("--------------------------------");
+
+
+
+
+            //Tabular Format
+            System.out.printf("%-5d %-30s %-10.2f %-10d%n",
+                    productId, productName, unitPrice, unitsInStock);
+
+
 
 
         }
